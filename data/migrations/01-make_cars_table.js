123c2fc0805e32cc854f4f2b3e8099ad20e1a6b3
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-17 19:41:11 -0400

    migration file complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,22 @@
-exports.up = function (knex) {
+exports.up = async function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.createTable('cars', tbl => {
+    tbl.increments('id');
+    tbl.text('vin', 120)
+      .unique()
+      .notNullable();
+    tbl.text('make', 120)
+      .notNullable();
+    tbl.text('model', 120)
+      .notNullable();
+    tbl.decimal('mileage')
+      .notNullable();
+    tbl.text('title');
+    tbl.text('transmission');
+  })
 };
 
-exports.down = function (knex) {
+exports.down = async function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.dropTableIfExists('cars');
 };
