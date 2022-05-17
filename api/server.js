a56f7be2bdc46484ebd1945502b7ab071afb2476
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-17 19:49:06 -0400

    working cars router and server

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express")
 
 const server = express()
+const carsRouter = require('./cars/cars-router');
 
 // DO YOUR MAGIC
+server.use(express.json());
+
+server.use('/api/cars', carsRouter);
+
+server.use('*', (req, res) => {
+    res.status(404).json( {message: `${req.method} ${req.baseUrl} not found`});
+});
 
 module.exports = server
