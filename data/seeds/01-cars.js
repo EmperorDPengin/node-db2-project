895a6252774386a0b54a665edf29a510d82e5ac7
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-18 01:28:45 -0400

    finally migration and seed working

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,35 @@
 // STRETCH
+exports.seed = function(knex, Promise) {
+    // we want to remove all data before seeding
+    // truncate will reset the primary key each time
+    return knex('cars').truncate()
+      .then(function () {
+        // add data into insert
+        return knex('cars').insert([
+          { vin: "14WHW85WDFG",
+            make: "Ford",
+            model: "KAKA",
+            mileage: 3652.12,
+            title: "this ones",
+            transmission: "auto" },
+            { vin: "SGST8316QTJSG",
+            make: "Nissan",
+            model: "CRature",
+            mileage: 32.2,
+            title: "tes",
+            transmission: "auto" },
+            { vin: "W85WDFG",
+            make: "FErr",
+            model: "Fast",
+            mileage: 36552.12,
+            title: "fasts",
+            transmission: "manual" },
+            { vin: "F83SWWJ8DFG",
+            make: "CAR",
+            model: "THE CAR",
+            mileage: 420.69,
+            title: "The Car IS a car",
+            transmission: "auto" },
+        ]);
+      });
+  };
